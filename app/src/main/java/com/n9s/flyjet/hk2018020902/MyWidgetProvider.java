8a35c2bb76cab450016915ef2b5546a85f5a598e
HKW <HKW@yahoo.com>
2018-02-09 14:52:56 +0800

    Initial commit

---
+++ app/src/main/java/com/n9s/flyjet/hk2018020902/MyWidgetProvider.java
@@ -0,0 +1,35 @@
+package com.n9s.flyjet.hk2018020902;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import java.util.Date;
+
+public class MyWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                R.layout.mywidget);
+        Date date = new Date();
+        Intent it = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context, 0, it, 0);
+        //remoteViews.setTextViewText(R.id.textView, date.toString());
+        remoteViews.setOnClickPendingIntent(R.id.button, pendingIntent);
+        ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
+// 取得 Widgets 管理器
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+// 透過管理器來對指定的Widgets 實例進行單項更新
+        manager.updateAppWidget(thisWidget, remoteViews);
+    }
+}
